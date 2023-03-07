1145147fc4919c7c2aaef770ec9000b6bcd70b18
Nik Volkov <vistusx93@gmail.com>
2023-03-07 13:50:32 +0300

    server returns 200

--- MockCamera/Program.cs
+++ MockCamera/Program.cs
@@ -1,24 +1,36 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 var tcpListener = new TcpListener(new IPAddress(new byte[4]{0,0,0,0}), 9898);
 tcpListener.Start();
 while (true)
 {
     var client = await tcpListener.AcceptTcpClientAsync();
-    Task.Run(() => Echo(client));
-    
+    await Task.Run(() => Echo(client));
 }
 
 async Task Echo(TcpClient client)
 {
     await using var clientStream = client.GetStream();
-    var buffer = new byte[1024];
-    int readBytes = 0;
-    while ((readBytes = await clientStream.ReadAsync(buffer)) != 0)
+    var buffer = new byte[128];
+    var request = string.Empty;
+    
+    while (await clientStream.ReadAsync(buffer) != 0)
     {
-        Console.WriteLine(System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length));
-    } 
+        var reqPart = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+        request += reqPart;
+        //Console.WriteLine(reqPart);
+
+        if (request.Contains($"{Environment.NewLine}{Environment.NewLine}"))
+        {
+            var okResponse = $"HTTP/1.1 200 OK{Environment.NewLine}{Environment.NewLine}";
+            var response = Encoding.UTF8.GetBytes(okResponse);
+            await clientStream.WriteAsync(response);
+            break;
+        }
+    }
+    
     client.Close();
     client.Dispose();
 }
