a3e3ed089c679053cb443653c7375f1aed7d34fe
Nik Volkov <vistusx93@gmail.com>
2023-03-07 17:36:59 +0300

    read buffer from start to last char

--- MockCamera/Program.cs
+++ MockCamera/Program.cs
@@ -2,26 +2,32 @@
 using System.Net.Sockets;
 using System.Text;
 
-var tcpListener = new TcpListener(new IPAddress(new byte[4]{0,0,0,0}), 9898);
+var tcpListener = new TcpListener(new IPAddress(new byte[]{0,0,0,0}), 9898);
 tcpListener.Start();
 while (true)
 {
     var client = await tcpListener.AcceptTcpClientAsync();
+    if (client is null)
+    {
+        continue;
+    }
     await Task.Run(() => Echo(client));
 }
 
 async Task Echo(TcpClient client)
 {
     await using var clientStream = client.GetStream();
-    var buffer = new byte[128];
+    var buffer = new byte[50];
     var request = new StringBuilder();
-    
-    while (await clientStream.ReadAsync(buffer) != 0)
+    var read = 0;
+    while ((read = await clientStream.ReadAsync(buffer)) != 0)
     {
-        var reqPart = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+        var reqPart = Encoding.UTF8.GetString(buffer, 0, read);
         request.Append(reqPart);
-        Console.WriteLine(reqPart);
+        Array.Clear(buffer, 0, buffer.Length);
+        Console.Write(reqPart);
 
+        //change to request
         if (reqPart.Contains($"{Environment.NewLine}{Environment.NewLine}"))
         {
             var okResponse = $"HTTP/1.1 200 OK{Environment.NewLine}{Environment.NewLine}";
